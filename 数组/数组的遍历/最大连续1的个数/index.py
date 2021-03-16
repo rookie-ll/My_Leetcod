9456a959c7aeecf52c64a4bfaeea34adedbc47ba
刘亮 <rookie_ll@126.com>
2021-03-16 11:27:52 +0800

    2021-03-16 添加‘最大连续1的个数’第二种解法

--- 数组/数组的遍历/最大连续1的个数/index.py
+++ 数组/数组的遍历/最大连续1的个数/index.py
@@ -11,15 +11,36 @@ class Solution(object):
             # 如果等于1 则累加res
             if i == 1:
                 res += 1
-            else:
+            if i != 1:
                 # 如果不等于1，则为0，,如果max_nu存储的数小于res，那么使用max_nu把连续的数存储起来
                 if max_nu < res:
                     max_nu = res
                 # 把res初始化为0
                 res = 0
+        if max_nu < res:
+            max_nu = res
         return max_nu
+
+    def findMaxConsecutiveOnes1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # 求最大连续的1的个数，数组中只有1和0
+        # 第二种方法
+        max_ls = []
+        nu = 0
+        for i in nums:
+            if i == 1:
+                nu += 1
+            else:
+                max_ls.append(nu)
+                nu = 0
+        max_ls.append(nu)
+        return max(max_ls)
+
 
 
 if __name__ == '__main__':
     s = Solution()
-    print(s.findMaxConsecutiveOnes([1, 0, 1, 0, 1, 1, 0,1]))
+    print(s.findMaxConsecutiveOnes1([1, 0, 1, 1]))
